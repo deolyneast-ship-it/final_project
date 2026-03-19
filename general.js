ac97a72a85d7a522b7b602fdb9ed994bb4f2188e
Deolyn East <deolyneast@gmail.com>
2026-03-19 09:14:36 +0200

    Update general.js

--- general.js
+++ general.js
@@ -1,8 +1,62 @@
+const express = require('express');
+let books = require("./booksdb.js");
+let isValid = require("./auth_users.js").isValid;
+let users = require("./auth_users.js").users;
+const public_users = express.Router();
+
+// Task 10: Get the book list available in the shop using Async/Await
 public_users.get('/', async function (req, res) {
   try {
-    const response = await axios.get('http://localhost:5000/books'); // Simulated endpoint
-    res.status(200).json(response.data);
+    const getBooks = () => new Promise((resolve) => resolve(books));
+    const allBooks = await getBooks();
+    res.status(200).send(JSON.stringify(allBooks, null, 4));
   } catch (error) {
     res.status(500).json({message: "Error retrieving books"});
   }
 });
+
+// Task 11: Get book details based on ISBN using Promises
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  new Promise((resolve, reject) => {
+    if (books[isbn]) {
+      resolve(books[isbn]);
+    } else {
+      reject("Book not found");
+    }
+  })
+  .then((book) => res.status(200).send(JSON.stringify(book, null, 4)))
+  .catch((err) => res.status(404).json({message: err}));
+});
+
+// Task 12: Get book details based on author using Promises
+public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author;
+  new Promise((resolve, reject) => {
+    let filteredBooks = Object.values(books).filter(b => b.author === author);
+    if (filteredBooks.length > 0) {
+      resolve(filteredBooks);
+    } else {
+      reject("No books found for this author");
+    }
+  })
+  .then((data) => res.status(200).send(JSON.stringify(data, null, 4)))
+  .catch((err) => res.status(404).json({message: err}));
+});
+
+// Task 13: Get all books based on title using Promises
+public_users.get('/title/:title', function (req, res) {
+  const title = req.params.title;
+  new Promise((resolve, reject) => {
+    let filteredBooks = Object.values(books).filter(b => b.title === title);
+    if (filteredBooks.length > 0) {
+      resolve(filteredBooks);
+    } else {
+      reject("No books found with this title");
+    }
+  })
+  .then((data) => res.status(200).send(JSON.stringify(data, null, 4)))
+  .catch((err) => res.status(404).json({message: err}));
+});
+
+module.exports.general = public_users;
